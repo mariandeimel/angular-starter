89c8b1b62e81bf6745527eaa5854cb39d03fcb16
marian.deimel@github.com <marian.deimel@github.com>
2023-08-01 13:37:14 +0200

    added xsrf interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ErrorInterceptor } from '@core/interceptors/error.interceptor';
 import { APP_ROUTES } from './app.routes';
+import { GlobalHttpInterceptor } from '@core/interceptors/global.interceptor';
+import { XsrfHttpInterceptor } from '@core/interceptors/xsrf.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: GlobalHttpInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: XsrfHttpInterceptor, multi: true },
   ]
-};
+}
