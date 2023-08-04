4795e1d50d03923e20fd0243f0c3a915267e383c
marian.deimel@github.com <marian.deimel@github.com>
2023-08-04 11:25:20 +0200

    detail facade

---
+++ src/app/core/services/detail-facade.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from "@angular/core"
+import { ApiService } from "./api.service"
+import { DetailStateService } from "./detail-state.service"
+import { finalize } from "rxjs"
+import { Identifiable } from "@core/interfaces/identifiable"
+
+@Injectable({ providedIn: 'root' })
+export class DetailFacadeService<T extends Identifiable> {
+
+  constructor(public state: DetailStateService<T>, public api: ApiService<T>) { }
+
+  show(id: number) {
+    this.state.loading.set(true)
+
+    this.api.show(id, true).pipe(
+      finalize(() => {
+        this.state.loading.set(false)
+      })
+    ).subscribe({
+      next: (response) => {
+        this.state.set(response)
+      },
+      error: (error) => {
+        this.state.error.set(error.error.message)
+      }
+    })
+  }
+
+  update(item: T) {
+    this.state.loading.set(true)
+
+    this.api.update(item.id, item, true).pipe(
+      finalize(() => {
+        this.state.loading.set(false)
+      })
+    ).subscribe({
+      next: (response) => {
+        this.state.update(response)
+      },
+      error: (error) => {
+        this.state.error.set(error.error.message)
+      }
+    })
+  }
+
+  delete(item: T) {
+    this.state.loading.set(true)
+
+    this.api.delete(item.id).pipe(
+      finalize(() => {
+        this.state.loading.set(false)
+      })
+    ).subscribe({
+      next: () => {
+        this.state.delete()
+      },
+      error: (error) => {
+        this.state.error.set(error.error.message)
+      }
+    })
+  }
+}
---
+++ src/app/core/services/detail-state.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+
+@Injectable({ providedIn: 'root' })
+export class DetailStateService<T> {
+  loading = signal(false)
+  item = signal<T | undefined>(undefined)
+  error = signal<string | null>(null)
+
+  item$ = toObservable(this.item)
+  loading$ = toObservable(this.loading)
+  error$ = toObservable(this.error)
+
+  set(item: T) {
+    this.item.set(item)
+  }
+
+  delete() {
+    this.item.set(undefined)
+  }
+
+  update(item: T) {
+    this.item.set(item)
+  }
+}
